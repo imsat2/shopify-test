7d45e723f559994046a3a2e23e0a777c5a1e4258
imsat2 <satyajit@devsnest.llc>
2024-08-23 11:36:41 +0600

    inertia install

---
+++ resources/js/Pages/Home.jsx
@@ -0,0 +1,9 @@
+export default function Home() {
+    return (
+        <>
+            <h1>Blog</h1>
+            <hr/>
+            <a href="/posts">Posts</a>
+        </>
+    )
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
 
 Route::get('/', function () {
-    return view('welcome');
+    return Inertia::render('Home');
 });
+
+Route::resource('/posts', PostController::class);
